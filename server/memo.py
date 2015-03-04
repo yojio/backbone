ee24aed8929c6a17584e30ac898b66f5130f25c5
yojio <yojio.jp@gmail.com>
2015-03-05 03:02:38 +0900

    変更

--- server/memo.py
+++ server/memo.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import webapp2
 import json
-import models
+
 from google.appengine.ext import ndb
+import webapp2
+
+import models
 
 
 class MemoHandler(webapp2.RequestHandler):
 
 
-    def get(self,key=None):
+    def get(self, key = None):
 
         if key:
-            memo = ndb.Key(urlsafe=key).get()
+            memo = ndb.Key(urlsafe = key).get()
             self.response.headers['Content-Type'] = 'application/json'
             self.response.out.write(json.dumps(memo.to_dict()))
         else:
@@ -20,7 +22,7 @@ class MemoHandler(webapp2.RequestHandler):
             self.response.headers['Content-Type'] = 'application/json'
             self.response.out.write(json.dumps([memo.to_dict() for memo in memos]))
 
-    def post(self,key=None):
+    def post(self, key = None):
         data = json.loads(self.request.body)
         memo = models.Memo(**data)
         memo.put()
@@ -28,17 +30,17 @@ class MemoHandler(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(json.dumps(memo.to_dict()))
 
-    def put(self,key=None):
+    def put(self, key = None):
         data = json.loads(self.request.body)
-        memo = ndb.Key(urlsafe=key).get()
+        memo = ndb.Key(urlsafe = key).get()
         memo.content = data["content"]
         memo.put()
 
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(json.dumps(memo.to_dict()))
 
-    def delete(self,key=None):
-        memo = ndb.Key(urlsafe=key).get()
+    def delete(self, key = None):
+        memo = ndb.Key(urlsafe = key).get()
         memo.key.delete()
 
         self.response.headers['Content-Type'] = 'application/json'
