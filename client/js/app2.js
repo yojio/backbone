6b650efbe85c4e52b21be820fa95894bb8f76b78
yojio <yojio.jp@gmail.com>
2015-03-04 22:10:56 +0900

    更新

---
+++ client/js/app2.js
@@ -0,0 +1,167 @@
+define([ 'underscore', 'backbone' ], function(_, Backbone) {
+
+	var Memo = Backbone.Model.extend({
+		idAttribute : "key",
+		defaults : {
+			"content" : ""
+		},
+		validate : function(attributes) {
+			if (attributes.content == "") {
+				return "content must be not empty.";
+			}
+		}
+	});
+
+	var MemoList = Backbone.Collection.extend({
+		model : Memo,
+		url : "/memo"
+	});
+
+	var memoList = new MemoList();
+
+	var observer = {
+		showArguments : function() {
+			console.log("+++observer.showArguments: ");
+			_.each(arguments, function(item, index) {
+				console.log("  +++arguments[" + index + "]: "
+						+ JSON.stringify(item));
+			})
+		}
+	}
+
+	_.extend(observer, Backbone.Events);
+
+	observer.listenTo(memoList, "all", observer.showArguments);
+
+	var memo = new Memo({
+		content : "Acroquest"
+	});
+
+	console.log("add");
+	memoList.add(memo);
+
+	console.log("change");
+	memo.set({
+		content : "Acroquest Technology"
+	});
+
+	console.log("remove");
+	memoList.remove(memo);
+
+	console.log("reset");
+	memoList.add([ new Memo({
+		content : "Acro1"
+	}), new Memo({
+		content : "Acro2"
+	}), new Memo({
+		content : "Acro3"
+	}) ]);
+
+	console.log("Before reset; " + JSON.stringify(memoList));
+
+	memoList.reset([ new Memo({
+		content : "Acro"
+	}), new Memo({
+		content : "Technology"
+	}), new Memo({
+		content : "Acroquest"
+	}) ]);
+
+	console.log("After reset; " + JSON.stringify(memoList));
+
+	console.log("sort");
+
+	memoList.comparator = function(item) {
+		return item.get("content");
+	};
+
+	memoList.sort();
+
+	console.log("After sort: " + JSON.stringify(memoList));
+
+	console.log("request, sync");
+
+	memo = new Memo({
+		content : "Murata"
+	}, {
+		collection : memoList
+	});
+
+	console.log("create");
+	memo.save(
+			null,
+			{
+				success : function() {
+					console.log("After create memoList: "
+							+ JSON.stringify(memoList));
+					console.log("After create memoList.length: "
+							+ JSON.stringify(memoList.length));
+				}
+			}).then(
+			function() {
+				console.log("fetch");
+				return memoList.fetch({
+					success : function() {
+						console.log("After fetch memoList: "
+								+ JSON.stringify(memoList));
+						console.log("After fetch memoList.length: "
+								+ JSON.stringify(memoList.length));
+					}
+				});
+			}).then(
+			function() {
+				var tempMemo = memoList.find(function(item) {
+					return item.get("content") === "Murata";
+				})
+
+				console.log("invalid");
+				tempMemo.save({
+					content : ""
+				});
+
+				console.log("invalid wait:true");
+				tempMemo.save({
+					content : ""
+				}, {
+					watit : true
+					//,silent : true
+				});
+
+				console.log("re-save");
+
+				return tempMemo.save({
+					content : "Kenichiro"
+				}, {
+					success : function() {
+						console.log("After save memoList: "
+								+ JSON.stringify(memoList));
+						console.log("After save memoList.length: "
+								+ JSON.stringify(memoList.length));
+					}
+				});
+
+			}).done(
+			function() {
+				console.log("destroy");
+
+				var tempMemo = memoList.find(function(item) {
+					return item.get("content") === "Kenichiro";
+				});
+
+				return tempMemo.destroy({
+					success : function() {
+						console.log("After destroy memoList:"
+								+ JSON.stringify(memoList));
+						console.log("After destory memoList.length:"
+								+ JSON.stringify(memoList));
+					}
+				});
+				observer.stopListening();
+			});
+
+	memoList.add(memo);
+
+	console.log("After add memo:" + JSON.stringify(memo));
+	console.log("After add memoList.length: " + memoList.length);
+
+});
--- server/memo.py
+++ server/memo.py
@@ -10,9 +10,15 @@ class MemoHandler(webapp2.RequestHandler):
 
 
     def get(self,key=None):
-        memo = ndb.Key(urlsafe=key).get()
-        self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write(json.dumps(memo.to_dict()))
+
+        if key:
+            memo = ndb.Key(urlsafe=key).get()
+            self.response.headers['Content-Type'] = 'application/json'
+            self.response.out.write(json.dumps(memo.to_dict()))
+        else:
+            memos = models.Memo.query().order(models.Memo.key)
+            self.response.headers['Content-Type'] = 'application/json'
+            self.response.out.write(json.dumps([memo.to_dict() for memo in memos]))
 
     def post(self,key=None):
         data = json.loads(self.request.body)
@@ -35,3 +41,5 @@ class MemoHandler(webapp2.RequestHandler):
         memo = ndb.Key(urlsafe=key).get()
         memo.key.delete()
 
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(memo.to_dict()))
